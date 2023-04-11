21b5882097488c3ee879faf2f6dcf20960da5914
Muhammad Waleed <72031196+muhwaleedch@users.noreply.github.com>
2023-04-11 14:33:45 +0500

    logo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,13 @@
+import { Head } from 'next/document';
 import Theme from '../styles/theme';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
+        <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
         <Component {...pageProps} />
       </Theme>
     </>
