1f34282bdcd09f594962e8d46ac1730899320122
Muhammad Waleed <72031196+muhwaleedch@users.noreply.github.com>
2023-04-12 11:29:34 +0500

    intro

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,7 +11,24 @@ const Hero = (props) => (
         <SectionTitle main center>
           Welcome To <br />
           My Personal Portfolio
+          <p className="intro-subtitle">
+                <span className="text-slider-items"></span>
+                <strong className="text-slider">
+                  <Typed
+                    strings={[
+                      "Front End Developer",
+                      "Back End Developer",
+                      "Software Engineer"
+                    ]}
+                    typeSpeed={80}
+                    backDelay={1100}
+                    backSpeed={30}
+                    loop
+                  />
+                </strong>
+              </p>
         </SectionTitle>
+        
         <SectionText>
         The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
         </SectionText>
